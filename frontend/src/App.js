0366edb40325a265164d2cd8dde7306f9881351a
albertohuerta <albertohuerta22@gmail.com>
2022-07-03 21:48:10 +0100

    imported react boostrap; bootswatch theme and file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
-      <h1>Welcome To ProShop</h1>
+      <main>
+        <Header />
+        <h1>Welcome To ProShop</h1>
+        <Footer />
+      </main>
     </>
   );
 }
