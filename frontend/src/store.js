d35f2f0f5c725279713f49bb544361633c2abb33
albertohuerta <albertohuerta22@gmail.com>
2022-07-08 23:42:30 +0100

    updated redux page, introduced bug, mongo failing to connect

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,11 +3,10 @@ import { combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-
-const reducer = combineReducers({})
-const initialState = {}
-
+const reducer = combineReducers({});
+const initialState = {};
 
 const store = configureStore({
-  reducer:
+  reducer: {},
+  initialState,
 });
