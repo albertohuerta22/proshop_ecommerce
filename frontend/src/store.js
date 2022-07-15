fc1ca1e3cf7016c0dcab3407c5e6874dd59045b4
albertohuerta <albertohuerta22@gmail.com>
2022-07-15 16:33:54 +0100

    created cart screen, created cart constant, created cart reducer, added reducer to store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,6 +3,7 @@ import {
   productDetailsReducer,
   productListReducer,
 } from './reducers/productReducer';
+import { cartReducer } from './reducers/cartReducers';
 
 const preloadedState = {};
 
@@ -10,6 +11,7 @@ const store = configureStore({
   reducer: {
     productList: productListReducer,
     productDetails: productDetailsReducer,
+    cart: cartReducer,
   },
   preloadedState,
 });
