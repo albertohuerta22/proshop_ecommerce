85c0acd95e4187711c6c752a7aa9e902b7772924
albertohuerta <albertohuerta22@gmail.com>
2022-07-17 15:07:49 +0100

    created controller file in backend, reduced routes with controllers

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,37 +1,19 @@
 import express from 'express';
-import asyncHandler from 'express-async-handler';
-import Product from '../models/productModel.js';
+import {
+  getProducts,
+  getProductById,
+} from '../controllers/productControllers.js';
 
 const router = express.Router();
 
 //@description  Fetch all products
 //@route  GET /api/products
 //@access PUBLIC
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    // mongoose middle ware express async handler
-    const products = await Product.find({}); // mongoose func returns promise
-
-    res.json(products);
-  })
-);
+router.route('/').get(getProducts);
 
 //@description  Fetch single product
 //@route  GET /api/products/:id
 //@access PUBLIC
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error('Product not found');
-    }
-  })
-);
+router.route('/').get(getProductById);
 
 export default router;
